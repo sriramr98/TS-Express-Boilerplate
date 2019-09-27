7b860696ade52266dbee311f7151260f7f640924
sriramr98 <sriramr083@gmail.com>
2019-09-27 14:14:08 +0530

    removed unwanted dependencies

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,11 +3,7 @@ class Config {
   static MONGODB_URI: string =
     process.env.MONGODB_URI ||
     `mongodb://${process.env.MONGODB_USERNAME ||
-      'localhost'}:27017/ts-boilerplate`;
-  static SALT_ROUNDS: number = parseInt(process.env.SALT_ROUNDS || '10');
-  static JWT_SECRET: string = process.env.JWT_SECRET || 'somejibberishsecret';
-  static JWT_EXPIRES_IN: string = process.env.JWT_EXPIRES_IN || '1d';
-  static JWT_REFRESH_EXPIRES_IN = process.env.JWT_REFRESH_EXPIRES_IN || '10d';
+    'localhost'}:27017/ts-boilerplate`;
   static FIREBASE_ADMIN_DB_URL = process.env.FIREBASE_ADMIN_DB_URL;
 }
 
